2dc0d68863739c62fcf3cfdec8ba2d19ea72e453
Sivaganesh Subramanian <209899@ibsplc.com>
2024-11-06 18:23:22 +0530

    create delivery service endpoint

---
+++ src/main/java/com/example/booking/delivery/controller/DeliveryController.java
@@ -0,0 +1,37 @@
+package com.example.booking.delivery.controller;
+
+import com.example.booking.delivery.dto.InvoiceDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+@RequestMapping("/api/delivery")
+public class DeliveryController {
+
+    private final Random random = new Random();
+
+    @PostMapping("/invoice")
+    public ResponseEntity<String> processInvoice(@RequestBody InvoiceDto invoiceDto) {
+
+        // Generates a random number between 0 and 3
+        int randomValue = random.nextInt(5);
+        return switch (randomValue) {
+//            case 0 -> ResponseEntity.ok("Success");
+            case 0 -> ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Fail");
+            case 1 -> ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Fail");
+            case 2 -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+            case 3 -> ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Service Not Available");
+            case 4 -> ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+            default -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unknown Error");
+        };
+
+    }
+
+
+}
